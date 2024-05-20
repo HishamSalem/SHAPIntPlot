ea5bf063fbaf7c25ba9afa18637720a0a1ebfc7e
HishamSalem <63689667+HishamSalem@users.noreply.github.com>
2024-05-20 19:51:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     description='A package for SHAP value interaction analysis and visualization',
     author='Your Name',
-    author_email='your.email@example.com',
+    author_email='hishamsalem99@hotmail.com',
     url='https://github.com/HishamSalem/SHAP_Interactions',
     packages=find_packages(),
     install_requires=[
