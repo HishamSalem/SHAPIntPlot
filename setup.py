50f73d6b9184a7d45512c93b2e94f0e9c00e9fbf
HishamSalem <63689667+HishamSalem@users.noreply.github.com>
2024-05-20 19:39:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_shap_package',
+    name='SHAPIntPlot',
     version='0.1.0',
     description='A package for SHAP value interaction analysis and visualization',
     author='Your Name',
